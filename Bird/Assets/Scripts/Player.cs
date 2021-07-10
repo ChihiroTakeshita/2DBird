16e5c29ae6b86dd3aad05296ec59d8cacefa32ee
Chi-ni <dokoitsu0506@gmail.com>
2021-07-10 15:18:14 +0900

    リポジトリ作成、アニメーション切り替えはできてる

---
+++ Bird/Assets/Scripts/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float JumpV = 10; //ジャンプ力
+
+    Rigidbody2D rb2D;
+    SpriteRenderer rend;
+    Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TouchManager.Began += (info) =>
+        {
+            rb2D.velocity = Vector2.zero; //落下速度をリセット
+            rb2D.AddForce(transform.up * JumpV, ForceMode2D.Impulse); //上方向に力を加える
+        };
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        StartCoroutine(ChangeSprite());
+    }
+
+    private IEnumerator ChangeSprite()
+    {
+        anim.SetBool("isDead", true);
+
+        yield return new WaitForSeconds(1.0f);
+
+        anim.SetBool("isDead", false);
+    }
+}
---
+++ Bird/Assets/Scripts/WallManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallManager : MonoBehaviour
+{
+    [SerializeField] private float interval = 1;
+    [SerializeField] GameObject[] walls;
+    [SerializeField] GameObject spawner;
+
+    float nextSpawnTime = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(nextSpawnTime < Time.timeSinceLevelLoad)
+        {
+            nextSpawnTime = Time.realtimeSinceStartup + interval;
+            int id = Random.Range(0, walls.Length);
+            GameObject obj = (GameObject)Instantiate(walls[id], spawner.transform.position, spawner.transform.rotation);
+        }
+    }
+}
