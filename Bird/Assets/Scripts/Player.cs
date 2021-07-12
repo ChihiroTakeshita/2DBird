31fd01569242e7b9ce48081ea5c7749732e21737
Chi-ni <dokoitsu0506@gmail.com>
2021-07-12 13:58:40 +0900

    壁に当たった時に操作不能になりました

--- Bird/Assets/Scripts/Player.cs
+++ Bird/Assets/Scripts/Player.cs
@@ -7,8 +7,9 @@ public class Player : MonoBehaviour
     [SerializeField] float JumpV = 10; //ジャンプ力
 
     Rigidbody2D rb2D;
-    SpriteRenderer rend;
     Animator anim;
+
+    bool isDead = false;
 
     // Start is called before the first frame update
     void Start()
@@ -20,24 +21,29 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        TouchManager.Began += (info) =>
-        {
-            rb2D.velocity = Vector2.zero; //落下速度をリセット
-            rb2D.AddForce(transform.up * JumpV, ForceMode2D.Impulse); //上方向に力を加える
-        };
+            TouchManager.Began += (info) =>
+            {
+                if(!isDead)
+                {
+                    rb2D.velocity = Vector2.zero; //落下速度をリセット
+                    rb2D.AddForce(transform.up * JumpV, ForceMode2D.Impulse); //上方向に力を加える
+                }
+            };
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        StartCoroutine(ChangeSprite());
+        StartCoroutine(ChangeAnimation());
     }
 
-    private IEnumerator ChangeSprite()
+    private IEnumerator ChangeAnimation()
     {
         anim.SetBool("isDead", true);
+        isDead = true;
 
         yield return new WaitForSeconds(1.0f);
 
         anim.SetBool("isDead", false);
+        isDead = false;
     }
 }
