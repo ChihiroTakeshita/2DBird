0c63028df1528a09185085a4601e52bcc130c69f
Chi-ni <dokoitsu0506@gmail.com>
2021-07-12 14:25:10 +0900

    1秒ごとに1点増えるスコアを追加

---
+++ Bird/Assets/Scripts/Score.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    [SerializeField] int scorePerSecond;
+
+    int currentScore = 0;
+    Text scoreText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        scoreText = this.GetComponent<Text>();
+        StartCoroutine(AddScore());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = currentScore.ToString();
+    }
+
+    private IEnumerator AddScore()
+    {
+        while(true)
+        {
+            currentScore += scorePerSecond;
+
+            yield return new WaitForSeconds(1.0f);
+        }
+    }
+}
